3dd40dcaae3f1f91df53f79e9b8e9cd32fe8c9f7
이춘식 <choonsik@gmail.com>
2020-10-22 02:44:16 +0000

    밥 먹고 하자

--- code/test.py
+++ code/test.py
@@ -5,3 +5,23 @@ for i in range(1, 10):
 
 a: str = 1
 print(type(a))
+
+
+print([n * 2 for n in range(1, 10 + 1) if n % 2 == 1])
+
+
+def get_natural_number():
+    n = 0
+    while True:
+        n += 1
+        yield n
+
+
+a = get_natural_number()
+
+print(a)
+
+for i in range(0, 10):
+    print(next(a))
+
+
